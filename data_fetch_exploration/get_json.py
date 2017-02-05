2df68ce844fa2a2140dd888249e4d3f7856d5fce
carlsednaoui <carl@Carls-MacBook-Air.local>
2017-02-04 22:06:13 -0500

    Re-arrange repo

--- data_fetch_exploration/get_json.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-URL = "https://s3.amazonaws.com/assets.mailcharts.com/~emails:ea084f6d-0d2e-35fa-8606-b9f1f0b00b79.json"
-
-resolve = requests.get(URL)
-print(resolve.json().keys())
-print(resolve.json()['subject'])
-print(resolve.json()['body-html'])
-
--- module_exploration/__init__.py
+++
@@ -1,4 +0,0 @@
-# the __init__.py file tells python this folder is a package
-
-# go up one directory and run
-# $python -m module_exploration.forest
