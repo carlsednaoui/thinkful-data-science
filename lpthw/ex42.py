370fee8f10803a8593c8fb9a62f19e2a54a13073
Carl Sednaoui <carlsednaoui@gmail.com>
2017-07-13 08:13:07 -0400

    Mentor session notes

---
+++ lpthw/ex42.py
@@ -0,0 +1,76 @@
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
+
+## Dog is-a Animal
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## Dog has-a name
+        self.name = name
+
+## Cat is-a animal
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## Cat has-a name
+        self.name = name
+
+## Person is-a object
+class Person(object):
+
+    def __init__(self, name):
+        ## Persona has-a name
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## Employee is-a Person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## Employess has-a salary
+        self.salary = salary
+
+## Fish is-a object
+class Fish(object):
+    pass
+
+## Salmon is-a fish
+class Salmon(Fish):
+    pass
+
+## Halibut is-a fish
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## satan is-a cat
+satan = Cat("Satan")
+
+## mary is-a person
+mary = Person("Mary")
+
+## mary has-a pet
+mary.pet = satan
+
+## frank is-a employee with a salary
+frank = Employee("Frank", 120000)
+
+## fank has-a pet
+frank.pet = rover
+
+## fipper is-a fish
+flipper = Fish()
+
+## crouse is-a salmon
+crouse = Salmon()
+
+## harry is-a halibut
+harry = Halibut()
